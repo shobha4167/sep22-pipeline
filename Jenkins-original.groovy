8f5dbe579e4ef512f4111f18856a60a4bc33577e
shobha4167 <shobha4167@gmail.com>
2022-09-30 17:40:37 +0530

    added scripts

--- Jenkins-original.groovy
+++ Jenkins-original.groovy
@@ -4,16 +4,30 @@ pipeline{
         stage("checkout"){
             steps{
                 echo "clone the source code"
-            }
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/shobha4167/Sep22-code.git']]])
+                sh "ls -la"
+                       } 
         }
         stage("Build"){
             steps{
                 echo "here we build source code"
+                sh ''' mvn clean package
+                    ls -ls target/
+                    '''
             }
         }
         stage("upload artifacts"){
             steps{
-                echo "upload artifacts to s3"
+                echo "upload artifacts to aws s3"
+                sh '''
+                aws s3 cp target/hello-*.war s3://sep22222/$JOB_NAME/master/$BUILD_NUMBER/
+                '''
+            }
+        }
+        stage("check artifacts"){
+            steps{
+                echo "check artifacts"
+                sh "aws s3 ls s3://sep22222/$JOB_NAME/master/$BUILD_NUMBER/"
             }
         }
     }
